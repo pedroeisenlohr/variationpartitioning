429851da8fdb779d6043ecacdefd154effb32d2b
Pedro Eisenlohr <pedrov.eisenlohr@gmail.com>
2018-10-29 11:59:55 -0300

    Update Modelling species distribution with tb-RDA.R

--- Modelling species distribution with tb-RDA.R
+++ Modelling species distribution with tb-RDA.R
@@ -74,7 +74,7 @@ dim(scores)
 ### before proceeding with the next step. An interesting way to address this
 ### issue is applying Hierarchical Clustering of Variables (Chavent et al. 2012).
 # Checking collinearities
-env.rda <- rda(scores, environment)
+env.rda <- rda(spp.h, environment)
 vif.cca(env.rda) ### Checking for collinearity. VIF should be < 10.
 # If VIF > 10, consider using Hierarchical Clustering of Variables, as follows:
 
@@ -131,19 +131,19 @@ vif.cca(env.rda) ### Checking for collinearity. VIF should be < 10.
 #################################################################
 
 ### IMPORTANT: The forward selection procedure suggested above uses 
-### "scores" as response variable matrix. This can lead to different
-### results in comparison to "spp.h" as response variable matrix.
+### "spp.h" as response variable matrix. This can lead to different
+### results in comparison to "scores" as response variable matrix.
 ### One possible strategy would be comparing the results of both models
 ### and select one of them based on an objective criterion (R² adjusted, 
 ### AIC etc.).
 
 # Forward selection of the environmental variables
-env.rda<-rda(scores,environment)
+env.rda<-rda(spp.h,environment)
 anova(env.rda, permutations = how(nperm=999))
 ### According to Blanchet et al. (2008): "If, and only if, the global 
 ### test is significant, one can proceed with forward selection"
 (env.R2a <- RsquareAdj(env.rda)$adj.r.squared)
-env.fwd <- packfor::forward.sel(scores, environment, adjR2thresh=env.R2a)
+env.fwd <- packfor::forward.sel(spp.h, environment, adjR2thresh=env.R2a)
 env.fwd #List of selected variables
 env.sign <- sort(env.fwd$order)
 (env.red <- environment[,c(env.sign)])
@@ -168,7 +168,7 @@ names(candidates)
 
 ### Optimization the selection of a subset of SWM among the candidates generated above,
 ### using the corrected significance threshold calculated ("forward"):
-(W_sel_fwd <- listw.select(scores, candidates, MEM.autocor = "positive", method = "FWD",
+(W_sel_fwd <- listw.select(spp.h, candidates, MEM.autocor = "positive", method = "FWD",
                     p.adjust = TRUE, MEM.all = TRUE, nperm = 999))
 ### Some characteristics of the best spatial model:
 # Best SWM:
@@ -202,7 +202,7 @@ dim(mem.all)
 #############################################
 ########## VARIATION PARTITIONING ###########
 #############################################
-scores.dudi <- dudi.pca(spp.h, scannf = F, nf=length.pca3)
+scores.dudi <- dudi.pca(spp.h, scannf = F, nf=length.pca3) #nf refers to the number of PCA components retained. Default is 2.
 
 vprda <- varipart(scores.dudi, env.red, spatial.red, nrepet=999)#classic variation partitioning
 vprda
@@ -219,7 +219,7 @@ res
 ### Testing the environmental significance, after considering #############  
 ################# the effect of selected MEMs #############################
 ###########################################################################
-env.rda<-rda(scores,env.red,spatial.red)
+env.rda<-rda(spp.h,env.red,spatial.red)
 summary(env.rda) ### Please observe the explanation of each axis.
 spenvcor(env.rda) # species-environment correlation
 intersetcor(env.rda) #"interset" correlation
@@ -257,19 +257,19 @@ flo<-data.frame(env.red$flo)
 pho<-data.frame(env.red$pho)
 
 #Testing significance of each predictor:
-env.rda.oxy <-rda(scores,oxy,cbind(slo,flo,pho,spatial.red))
+env.rda.oxy <-rda(spp.h,oxy,cbind(slo,flo,pho,spatial.red))
 summary(env.rda.oxy)
 (anova(env.rda.oxy,permutations = how(nperm=999)))
 
-env.rda.slo <-rda(scores,slo,cbind(oxy,flo,pho,spatial.red))
+env.rda.slo <-rda(spp.h,slo,cbind(oxy,flo,pho,spatial.red))
 summary(env.rda.slo)
 (anova(env.rda.slo,permutations = how(nperm=999)))
 
-env.rda.flo <-rda(scores,flo,cbind(oxy,slo,pho,spatial.red))
+env.rda.flo <-rda(spp.h,flo,cbind(oxy,slo,pho,spatial.red))
 summary(env.rda.flo)
 (anova(env.rda.flo,permutations = how(nperm=999)))
 
-env.rda.pho <-rda(scores,pho,cbind(oxy,flo,slo,spatial.red))
+env.rda.pho <-rda(spp.h,pho,cbind(oxy,flo,slo,spatial.red))
 summary(env.rda.pho)
 (anova(env.rda.pho,permutations = how(nperm=999)))
 
@@ -278,7 +278,7 @@ summary(env.rda.pho)
 ### Testing the spatial significance, after considering ###################  
 ################# the effect of selected env ##############################
 ###########################################################################
-spatial.rda<-rda(scores,spatial.red,env.red)
+spatial.rda<-rda(spp.h,spatial.red,env.red)
 summary(spatial.rda) ### Please observe the explanation of each axis.
 spenvcor(spatial.rda) # species-environment correlation
 intersetcor(spatial.rda) #"interset" correlation
