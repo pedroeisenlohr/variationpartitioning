066b0ed31fc393503b293fa09d2349cf2fd45b92
Pedro Eisenlohr <pedrov.eisenlohr@gmail.com>
2019-03-13 08:59:02 -0300

    Update Modelling species distribution with tb-RDA.R

--- Modelling species distribution with tb-RDA.R
+++ Modelling species distribution with tb-RDA.R
@@ -1,18 +1,17 @@
 ### SELECION OF ENVIRONMENTAL AND SPATIAL DATA, AND VARIATION PARTITIONING ###
-#By: Pedro V. Eisenlohr - UNEMAT, Alta Floresta
+#By: Pedro V. Eisenlohr - UNEMAT, Alta Floresta (pedro.eisenlohr@unemat.br)
 #Based on Bauman et al. (2018), Clappe et al. (2018), Blanchet et al. (2008), Chavent et al. (2012) and others
 
 # Acknowledgments:
 # Sylvie Clappe
 # João Carlos Pires de Oliveira
 
-
 library(ade4)
 library(adespatial)
 library(spdep)
 library(vegan)
-library(packfor)
 library(usdm)
+library(ape)
 
 ##Species matrix
 species<-read.table(file.choose(),row.names=1,header=T,sep=",") ###community species data (abundance or binary)
@@ -27,38 +26,33 @@ View(spp.h)
 ##Environmental matrix
 environment <- read.table(file.choose(),row.names=1,header=T,sep=",") #environmental variables
 View(environment)
-environment<-decostand(environment,"standardize") #caso queira padronizar a escala das variáveis ambientais
-View(environment)
+dim(environment)
+#environment<-decostand(environment,"standardize") #caso queira padronizar a escala das variáveis ambientais
+#View(environment)
 
 ##Spatial matrix
 ll<-read.table(file.choose(),row.names=1,header=T,sep=",") #longitude and latitude
 View(ll)
+dim(ll)
 
 
-
-#### CONDENSING SPECIES MATRIX (OPTIONAL) ##########
-(length.pca <- (nrow(spp.h)-1))
-pca.species.p <- dudi.pca(spp.h, scannf = F, nf = length.pca)
-summary(pca.species.p)
+#### CONDENSING SPECIES MATRIX WITH A PCoA (OPTIONAL) ##########
+spp.dist <- vegdist(spp.u, method="bray")
+pcoa.species <- cmdscale(spp.dist, k=(nrow(spp.u)-1), eig=TRUE)
+scores.pcoa<-pcoa.species$points
+head(scores.pcoa,30)
 # Eigenvalues
-(ev<-pca.species.p$eig)
+(ev<-pcoa.species$eig)
 
-# Apply Kaiser-Guttman criterion to select axes
-#(length.pca2<- length(ev[ev > mean(ev)]))
-#pca.species <- dudi.pca(spp.h, scannf = F, nf = length.pca2)
-#scores <- pca.species$l1
-#head(scores)
-#dim(scores)
-
-# Alternatively, you may retain axes that capture 95% of all inertia:
-(explic <- pca.species.p$eig/sum(pca.species.p$eig))
+# You may retain axes that capture 95% of all inertia:
+(explic <- pcoa.species$eig/sum(pcoa.species$eig))
 (cmu <- cumsum(explic))
-(length.pca3 <- which.min(cmu <= 0.95))
-pca.species <- dudi.pca(spp.h, scannf = F, nf = length.pca3)
-scores <- pca.species$l1
-head(scores)
-dim(scores)
-#################################
+(length.pcoa <- which.min(cmu <= 0.95))
+(scores.pcoa <- scores.pcoa[,1:length.pcoa])
+head(scores.pcoa,20)
+dim(scores.pcoa)
+##################################################################
+
 
 
 
@@ -74,7 +68,7 @@ dim(scores)
 ### before proceeding with the next step. An interesting way to address this
 ### issue is applying Hierarchical Clustering of Variables (Chavent et al. 2012).
 # Checking collinearities
-env.rda <- rda(spp.h, environment) #If you prefer to work with condensed response matrix, please change 'spp.h' by 'scores'
+env.rda <- rda(scores.pcoa, environment) #If you prefer to work with the whole response matrix, please change 'scores.pcoa' by 'spp.h'
 vif.cca(env.rda) ### Checking for collinearity. VIF should be < 10.
 # If VIF > 10, consider using Hierarchical Clustering of Variables, as follows:
 
@@ -87,7 +81,7 @@ vif.cca(env.rda) ### Checking for collinearity. VIF should be < 10.
 #library(ClustOfVar)
 #tree <- hclustvar(environment)
 #plot(tree)
-#stab <- stability(tree,B=100) #To help in the selection of the number of partitions.
+#stab <- stability(tree,B=999) #To help in the selection of the number of partitions.
 
 ### Here, you need to adjust the routine to the number of selected clusters.
 ### For example, if this number is 4:
@@ -130,24 +124,19 @@ vif.cca(env.rda) ### Checking for collinearity. VIF should be < 10.
 ### FORWARD SELECTION OF PREDICTORS
 #################################################################
 
-### IMPORTANT: The forward selection procedure suggested above uses 
-### "spp.h" as response variable matrix. This can lead to different
-### results in comparison to "scores" as response variable matrix.
-### One possible strategy would be comparing the results of both models
-### and select one of them based on an objective criterion (R² adjusted, 
-### AIC etc.).
-
 # Forward selection of the environmental variables
-env.rda<-rda(spp.h,environment) #If you prefer to work with condensed response matrix, please change 'spp.h' by 'scores'
+env.rda<-rda(scores.pcoa,environment) #If you prefer to work with the whole response matrix, please change 'scores.pcoa' by 'spp.h'
 anova(env.rda, permutations = how(nperm=999))
 ### According to Blanchet et al. (2008): "If, and only if, the global 
 ### test is significant, one can proceed with forward selection"
 (env.R2a <- RsquareAdj(env.rda)$adj.r.squared)
-env.fwd <- packfor::forward.sel(spp.h, environment, adjR2thresh=env.R2a)
+env.fwd <- packfor::forward.sel(scores.pcoa, environment, adjR2thresh=env.R2a)
 env.fwd #List of selected variables
 env.sign <- sort(env.fwd$order)
-(env.red <- environment[,c(env.sign)])
+env.red <- environment[,c(env.sign)]
+head(env.red)
 
+save.image()
 
 #############################################
 ######### SELECTING SPATIAL DATA ############
@@ -168,8 +157,8 @@ names(candidates)
 
 ### Optimization the selection of a subset of SWM among the candidates generated above,
 ### using the corrected significance threshold calculated ("forward"):
-(W_sel_fwd <- listw.select(spp.h, candidates, MEM.autocor = "positive", method = "FWD",
-                    p.adjust = TRUE, MEM.all = TRUE, nperm = 999)) 
+(W_sel_fwd <- listw.select(scores.pcoa, candidates, MEM.autocor = "positive", method = "FWD",
+                    p.adjust = TRUE, MEM.all = FALSE, nperm = 999)) 
 #If you prefer to work with condensed response matrix, please change 'spp.h' by 'scores' above.
 
 ### Some characteristics of the best spatial model:
@@ -180,7 +169,7 @@ W_sel_fwd$best.id
 SWM.selected<-W_sel_fwd$best$MEM.select
 class(SWM.selected)
 dim(SWM.selected)
-write.table(SWM.selected,"SWMselected.csv")
+#write.table(SWM.selected,"SWMselected.csv")
 # Write the selected MEMs to a new object
 spatial.red <- as.matrix(SWM.selected)
 class(spatial.red)
@@ -200,29 +189,36 @@ mem.all
 class(mem.all)
 dim(mem.all)
 
+### Obtaining positive spatial eigenvectors from the SWM selected above:
+mem.positive<-mem(candidates.sel,MEM.autocor = "positive")
+mem.positive
+class(mem.positive)
+dim(mem.positive)
+
 
 #############################################
 ########## VARIATION PARTITIONING ###########
 #############################################
-vprda <- varipart(spp.h, env.red, spatial.red, scale=TRUE) #classic variation partitioning
+vprda <- varipart(scores.pcoa, env.red, spatial.red, scale=TRUE) #classic variation partitioning
 vprda
 
-vprdaMSR <- msr(vprda, mem.all, nrepet = 999) #new variation partitioning 
+vprdaMSR <- msr(vprda, mem.all, nrepet = 999) #new variation partitioning (Clappe et al. 2018)
 vprdaMSR
 
 res <- rbind(vprda$R2.adj, vprdaMSR$R2.adj.msr)
+
 rownames(res) <- c("Standard VP", "MSR VP")
 res
 
 
-### Alternatively, one can use a condensed response matrix that retain 95% of total inertia ('pca.species'):
-#vprda <- varipart(pca.species, env.red, spatial.red, scale=TRUE) #classic variation partitioning
-#vprda
+### Alternatively, one can use the whole response matrix:
+#vprda.all <- varipart(ssp.h, env.red, spatial.red, scale=TRUE) #classic variation partitioning
+#vprda.all
 
-#vprdaMSR <- msr(vprda, mem.all, nrepet = 999) #new variation partitioning 
+#vprdaMSR <- msr(vprda.all, mem.all, nrepet = 999) #new variation partitioning 
 #vprdaMSR
 
-#res <- rbind(vprda$R2.adj, vprdaMSR$R2.adj.msr)
+#res <- rbind(vprda.all$R2.adj, vprdaMSR$R2.adj.msr)
 #rownames(res) <- c("Standard VP", "MSR VP")
 #res
 
@@ -230,7 +226,7 @@ res
 ####### Testing the environmental significance, after considering #########  
 ################# the effect of selected MEMs #############################
 ###########################################################################
-env.rda<-rda(spp.h,env.red,spatial.red)
+env.rda<-rda(scores.pcoa,env.red,spatial.red)
 head(summary(env.rda)) ### Please observe the explanation of each axis.
 spenvcor(env.rda) # species-environment correlation
 intersetcor(env.rda) #"interset" correlation
@@ -241,6 +237,9 @@ test.env
 ##############################################################################
 #### Model validation: checking for spatial independence of RDA residuals ####
 ##############################################################################
+
+##### Pending some adjustments recommended by David Bauman (personal contact) #####
+
 #Generating objects required for the analysis:
 X <- ll [,1]
 Y <- ll [,2]
@@ -254,7 +253,7 @@ cor.res<-dist(res)
 
 #Mantel correlogram with Spearman correlation
 (mite.correlog2 <- mantel.correlog(cor.res, XY=ll, cutoff=FALSE, 
-                                  r.type="spearman", nperm=9999))
+                                  r.type="spearman", nperm=99))
 summary(mite.correlog2)
 plot(mite.correlog2)
 
@@ -289,7 +288,7 @@ summary(env.rda.pho)
 ######## Testing the spatial significance, after considering ##############  
 ################# the effect of selected env ##############################
 ###########################################################################
-spatial.rda<-rda(spp.h,spatial.red,env.red)
+spatial.rda<-rda(scores.pcoa,spatial.red,env.red)
 head(summary(spatial.rda)) ### Please observe the explanation of each axis.
 spenvcor(spatial.rda) # species-space correlation
 intersetcor(spatial.rda) #"interset" correlation
@@ -302,13 +301,65 @@ test.spatial
 ### Evaluating the whole model ######
 ###########################################################################
 all.predictors <- cbind(env.red,spatial.red)
-all<-rda(spp.h,all.predictors)
+all<-rda(scores.pcoa,all.predictors)
 head(summary(all)) ### Please observe the explanation of each axis.
 teste.all<-anova(all, permutations = how(nperm=999))
 teste.all
 #plot(all)
 # To test each axis individually:
-rda.formula <- rda(spp.h~., data=all.predictors)
+rda.formula <- rda(scores.pcoa~., data=all.predictors)
 anova(rda.formula, by="axis")
+
+#Testing significance for each predictor.
+### In this case, the MEMs selected above were MEM3,MEM6,MEM4,MEM1,MEM9,MEM10,MEM2,MEM7,MEM16,MEM11,MEM8
+#Creating objects for each predictor in spatial.red:
+spatial.red<-as.data.frame(spatial.red)
+MEM3<-data.frame(spatial.red$MEM3)
+MEM6<-data.frame(spatial.red$MEM6)
+MEM4<-data.frame(spatial.red$MEM4)
+MEM1<-data.frame(spatial.red$MEM1)
+MEM9<-data.frame(spatial.red$MEM9)
+MEM10<-data.frame(spatial.red$MEM10)
+MEM2<-data.frame(spatial.red$MEM2)
+MEM7<-data.frame(spatial.red$MEM7)
+MEM16<-data.frame(spatial.red$MEM16)
+MEM11<-data.frame(spatial.red$MEM11)
+MEM8<-data.frame(spatial.red$MEM8)
+all.spatial<-cbind(MEM3,MEM6,MEM4,MEM1,MEM9,MEM10,MEM2,MEM7,MEM16,MEM11,MEM8)
+View(all.spatial)
+
+#Testing significance of each predictor:
+spatial.rda.MEM3 <-rda(scores.pcoa,MEM3,cbind(all.spatial[,-c(1)],env.red))
+(anova(spatial.rda.MEM3,permutations = how(nperm=999)))
+
+spatial.rda.MEM6 <-rda(scores.pcoa,MEM6,cbind(all.spatial[,-c(2)],env.red))
+(anova(spatial.rda.MEM6,permutations = how(nperm=999)))
+
+spatial.rda.MEM4 <-rda(scores.pcoa,MEM4,cbind(all.spatial[,-c(3)],env.red))
+(anova(spatial.rda.MEM4,permutations = how(nperm=999)))
+
+spatial.rda.MEM1 <-rda(scores.pcoa,MEM1,cbind(all.spatial[,-c(4)],env.red))
+(anova(spatial.rda.MEM1,permutations = how(nperm=999)))
+
+spatial.rda.MEM9 <-rda(scores.pcoa,MEM9,cbind(all.spatial[,-c(5)],env.red))
+(anova(spatial.rda.MEM9,permutations = how(nperm=999)))
+
+spatial.rda.MEM10 <-rda(scores.pcoa,MEM10,cbind(all.spatial[,-c(6)],env.red))
+(anova(spatial.rda.MEM10,permutations = how(nperm=999)))
+
+spatial.rda.MEM2 <-rda(scores.pcoa,MEM2,cbind(all.spatial[,-c(7)],env.red))
+(anova(spatial.rda.MEM2,permutations = how(nperm=999)))
+
+spatial.rda.MEM7 <-rda(scores.pcoa,MEM7,cbind(all.spatial[,-c(8)],env.red))
+(anova(spatial.rda.MEM7,permutations = how(nperm=999)))
+
+spatial.rda.MEM16 <-rda(scores.pcoa,MEM16,cbind(all.spatial[,-c(9)],env.red))
+(anova(spatial.rda.MEM16,permutations = how(nperm=999)))
+
+spatial.rda.MEM11 <-rda(scores.pcoa,MEM11,cbind(all.spatial[,-c(10)],env.red))
+(anova(spatial.rda.MEM11,permutations = how(nperm=999)))
+
+spatial.rda.MEM8 <-rda(scores.pcoa,MEM8,cbind(all.spatial[,-c(11)],env.red))
+(anova(spatial.rda.MEM8,permutations = how(nperm=999)))
 
 #END
