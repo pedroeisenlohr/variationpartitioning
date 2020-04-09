ba4b7e77a3bf25da98c339ab805f864c83c3f0c7
Pedro Eisenlohr <pedrov.eisenlohr@gmail.com>
2020-04-09 09:08:30 -0300

    Update Modelling species distribution with tb-RDA.R

--- Modelling species distribution with tb-RDA.R
+++ Modelling species distribution with tb-RDA.R
@@ -4,7 +4,7 @@
 
 # Acknowledgments:
 # Sylvie Clappe
-# João Carlos Pires de Oliveira
+# Jo?o Carlos Pires de Oliveira
 
 library(ade4)
 library(adespatial)
@@ -16,6 +16,7 @@ library(ape)
 setwd(choose.dir())
 dir()
 
+
 ##Species matrix
 species<-read.table(file.choose(),row.names=1,header=T,sep=",") ###community species data (abundance or binary)
 View(species)
@@ -23,81 +24,21 @@ dim(species)
 unicates<-apply(species,2,sum) 
 spp.u<-species[,which(unicates>1)] ###removing unicates
 dim(spp.u)
-#spp.h <- decostand(spp.u, method = "hell")
-#View(spp.h)
+spp.h <- decostand(spp.u, method = "hell")
+View(spp.h)
 
 ##Environmental matrix
 environment <- read.table(file.choose(),row.names=1,header=T,sep=",") #environmental variables
 View(environment)
 dim(environment)
-#environment<-decostand(environment,"standardize") #caso queira padronizar a escala das variáveis ambientais
-#View(environment)
+#environment_std<-decostand(environment,"standardize") #caso queira padronizar a escala das vari?veis ambientais
+#View(environment_std)
 
 ##Spatial matrix
 ll<-read.table(file.choose(),row.names=1,header=T,sep=",") #longitude and latitude
 View(ll)
 dim(ll)
 
-
-#### CONDENSING SPECIES MATRIX WITH A PCoA (OPTIONAL) ##########
-spp.dist <- vegdist(spp.u, method="bray") #Bray-Curtis dissimilarity index
-#library(recluster)
-#spp.dist.s <- recluster.dist(spp.u) #Simpson dissimilarity index
-pcoa.species <- cmdscale(spp.dist, k=(nrow(spp.u)-1), eig=TRUE)
-scores.pcoa<-pcoa.species$points
-head(scores.pcoa,30)
-# Eigenvalues
-(ev<-pcoa.species$eig)
-
-# You may retain axes that capture 95% of all inertia:
-(explic <- pcoa.species$eig/sum(pcoa.species$eig))
-(cmu <- cumsum(explic))
-(length.pcoa <- which.min(cmu <= 0.95))
-(scores.pcoa <- scores.pcoa[,1:length.pcoa])
-head(scores.pcoa,20)
-dim(scores.pcoa)
-
-# Alternatively, you may retain the significant axes (Broken-stick's approach):
-# Eigenvalues
-ev <- pcoa.species$eig
-(ev.r <- ev[1:30])
-# Broken stick model
-n <- length(ev)
-bsm <- data.frame(j=seq(1:n), p=0)
-bsm$p[1] <- 1/n
-for (i in 2:n)
-{
-  bsm$p[i] = bsm$p[i-1] + (1/(n + 1 - i))
-}
-bsm$p <- 100*bsm$p/n
-bsm
-dev.new(title="PCoA eigenvalues")
-barplot(t(cbind(100*ev/sum(ev),bsm$p[n:1])), beside=TRUE, 
-        main="% variance", col=c("bisque",2), las=2)
-legend("topright", c("% eigenvalue", "Broken stick model"), 
-       pch=15, col=c("bisque",2), bty="n")
-source("evplot.R")
-evplot(ev.r)  #se utilizar como argumento ev.r, somente utilize o gráfico inferior.
-              #Se utilizar como argumento ev, pode utilizar ambos os gráficos.
-# Alternatively, you may retain the significant axes (permutation's approach):
-#library(PCPS)
-#pcoa.sig <- pcoa.sig(spp.u, method="bray", axis=3, iterations=1000)
-
-# If, for instance, the first three axes are retained in the above analysis:
-(scores.pcoa.3 <- scores.pcoa[,1:3])
-head(scores.pcoa.3,20)
-dim(scores.pcoa.3)
-
-# Plot of the sites
-dev.new(title="PCoA")
-ordiplot(scores(pcoa.species, choices=c(1,2)), type="t", main="PCoA")
-abline(h=0, lty=3)
-abline(v=0, lty=3)
-# Add weighted average projection of species
-spe.wa <- wascores(pcoa.species$points[,1:2], spp.u)
-text(spe.wa, rownames(spe.wa), cex=0.7, col="red")
-write.table(spe.wa,"Species_Scores_PCoA-vazante.csv")
-##################################################################
 
 
 
@@ -116,7 +57,7 @@ vif(environment)
 # If VIF > 10, consider using Hierarchical Clustering of Variables, as follows:
 
 
-----------------------------------------------------------------------
+#----------------------------------------------------------------------#
 
 ######################################################################
 #### HIERARCHICAL CLUSTERING OF VARIABLES (Chavent et al. 2012) ######
@@ -124,40 +65,22 @@ vif(environment)
 library(ClustOfVar)
 tree <- hclustvar(environment)
 plot(tree)
-stab <- stability(tree,B=1000) #To help in the selection of the number of partitions.
-plot(stab)
+stab <- stability(tree,B=1000, graph = T) #To help in the selection of the number of partitions.
+(nc<-which.max(stab$meanCR)+1)
 
-### Here, you need to adjust the routine to the number of selected clusters.
-### For example, if this number is 22:
-P22<-cutreevar(tree,22,matsim=TRUE)
-cluster <- P22$cluster
+### Here, you need to adjust he routine to the number of selected clusters (if you don't want to use nc).
+P<-cutreevar(tree,nc,matsim=TRUE)
+cluster <- P$cluster
 X <- environment
-princomp(X[,which(cluster==1)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==2)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==3)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==4)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==5)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==6)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==7)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==8)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==9)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==10)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==11)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==12)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==13)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==14)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==15)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==16)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==17)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==18)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==19)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==20)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==21)],cor=TRUE)$sdev^2
-princomp(X[,which(cluster==22)],cor=TRUE)$sdev^2
-P22$cluster
-clusterID <- P22$var
+
+for (i in 1:nc) {
+  pc<-princomp(X[,which(cluster==i)],cor=TRUE)$sdev^2
+  print(pc)
+}
+P$cluster
+clusterID <- P$var
 clusterID
-write.table(P22$scores,"PCAScores.csv")
+write.table(P$scores,"PCAScores.csv")
 
 env2 <- read.table("PCAScores.csv",row.names=1,header=T,sep=" ")
 dim(env2)
@@ -166,10 +89,10 @@ vif(env2) #Be sure that no variable presents VIF>10.
 
 #In case of no colinearities, please run the command below:
 env3=env2
-#In case of collinearities, please run the commands below (or re-define the number of groups above):
+
+#In case of collinearities, please run the commands below (or re-define the number of groups below):
 #(v1<-vifcor(env2,th=0.8)) 
 #env3 <- exclude(env2, v1)
-#vif(env3)
 
 names(env3)
 write.table(env3,"env_without_collinearities.csv")
@@ -180,28 +103,28 @@ View(environment)
 #### END OF HIERARCHICAL CLUSTERING OF VARIABLES ################
 #################################################################
 
------------------------------------------------------------------
+#-----------------------------------------------------------------
 
 #################################################################
 ### FORWARD SELECTION OF PREDICTORS
 #################################################################
 
 # Forward selection of the environmental variables
-env.rda <- rda(scores.pcoa.3,environment) #If you prefer to work with the whole response matrix, please change 'scores.pcoa' by 'spp.h'
+env.rda <- rda(spp.h,environment) #If you prefer to work with the whole response matrix, please change 'scores.pcoa' by 'spp.h'
 env.rda
 anova(env.rda, permutations = how(nperm=999))
 ### According to Blanchet et al. (2008): "If, and only if, the global 
 ### test is significant, one can proceed with forward selection"
 (env.R2a <- RsquareAdj(env.rda)$adj.r.squared)
-env.fwd <- forward.sel(scores.pcoa.3, environment, adjR2thresh = env.R2a)
+env.fwd <- forward.sel(spp.h, environment, R2thresh=env.R2a)
 env.fwd #List of selected variables
 env.sign <- sort(env.fwd$order)
 env.red <- environment[,c(env.sign)]
 head(env.red)
-vif(env.red)
-env.rda.selected <- rda(scores.pcoa.3, env.red)
-(env.R2a.selected <- RsquareAdj(env.rda.selected)$adj.r.squared)
-save.image()
+#vif(env.red)
+#env.rda.selected <- rda(spp.h, env.red)
+#env.R2a <- RsquareAdj(env.rda.selected)$adj.r.squared)
+# save.image()
 
 #############################################
 ######### SELECTING SPATIAL DATA ############
@@ -222,10 +145,8 @@ names(candidates)
 
 ### Optimization the selection of a subset of SWM among the candidates generated above,
 ### using the corrected significance threshold calculated ("forward"):
-(W_sel_fwd <- listw.select(scores.pcoa.3, candidates, MEM.autocor = "positive", method = "FWD",
+(W_sel_fwd <- listw.select(spp.h, candidates, MEM.autocor = "positive", method = "FWD",
                     p.adjust = TRUE, MEM.all = FALSE, nperm = 999)) 
-#If you prefer to work with the whole response matrix, please change 'scores.pcoa.3' by 'spp.h' above.
-
 save.image()
 
 ### Some characteristics of the best spatial model:
@@ -240,7 +161,7 @@ dim(SWM.selected)
 # Write the selected MEMs to a new object
 spatial.red <- as.matrix(SWM.selected)
 class(spatial.red)
-space.rda.selected <- rda(scores.pcoa.3, spatial.red)
+space.rda.selected <- rda(spp.h, spatial.red)
 (env.R2a <- RsquareAdj(space.rda.selected)$adj.r.squared)
 
 # Creating an object with the name of the selected SWM:
@@ -259,83 +180,183 @@ class(mem.all)
 dim(mem.all)
 
 ### Obtaining positive spatial eigenvectors from the SWM selected above:
-mem.positive<-mem(candidates.sel,MEM.autocor = "positive")
-mem.positive
-class(mem.positive)
-dim(mem.positive)
+#mem.positive<-mem(candidates.sel,MEM.autocor = "positive")
+#mem.positive
+#class(mem.positive)
+#dim(mem.positive)
 
 save.image()
+
 
 
 #############################################
 ########## VARIATION PARTITIONING ###########
 #############################################
-vprda <- varipart(scores.pcoa.3, env.red, spatial.red, scale=TRUE) #classic variation partitioning
+source("varipart.R") 
+vprda <- varipart(spp.h, env.red, spatial.red) #classic variation partitioning
 vprda
 
-vprdaMSR <- msr(vprda, mem.all, nrepet = 999) #new variation partitioning (Clappe et al. 2018)
-vprdaMSR
-
-res <- rbind(vprda$R2.adj, vprdaMSR$R2.adj.msr)
-
-rownames(res) <- c("Standard VP", "MSR VP")
-res
+source("msrvaripart.R")
+vprdaMSR <- msr.varipart2(vprda, mem.all, nrepet = 999) #new variation partitioning (Clappe et al. 2018)
+vprdaMSR #observe o resultado para a fração [a]. Para acessar a significância da fração [c], considere o resultado obtido por vprda.
 
 
-### Alternatively, one can use the whole response matrix:
-#vprda.all <- varipart(spp.h, env.red, spatial.red, scale=TRUE) #classic variation partitioning
-#vprda.all
 
-#vprdaMSR <- msr(vprda, mem.all, nrepet = 999) #new variation partitioning 
-#vprdaMSR
+####################################################
+## Testing fraction [b] of variance partitioning ###
+####################################################
 
-#res <- rbind(vprda.all$R2.adj, vprdaMSR$R2.adj.msr)
-#rownames(res) <- c("Standard VP", "MSR VP")
-#res
+#Testing fraction [b] (Bauman et al. 2019)
+modsel.env <- listw.select(env.red, candidates, MEM.autocor = "positive", method = "global")
+(global.env <- anova(rda(spp.h, env.red))$Pr[1])#apenas checar se foi significativo. N?o precisa anotar. Se n?o for significativo, n?o prossiga.
+VP <- varpart(spp.h, env.red, spatial.red)
+# SSEF:
+(SSEF <- VP$part$indfract$Adj.R.square[2])
+SSEF.test <- envspace.test(spe = spp.h, env = env.red, coord = ll, MEM.spe = spatial.red,
+		listw.env = candidates[[modsel.env$best.id]], MEM.autocor = "positive", regular = FALSE, nperm = 999)
+# Significance of the permutation test performed with MSR:
+SSEF.test$pval
+
 
 ###########################################################################
-####### Testing the environmental significance, after considering #########  
+####### Assessing the environmental model, after considering #########  
 ################# the effect of selected MEMs #############################
 ###########################################################################
-env.rda<-rda(scores.pcoa.3,env.red,spatial.red)
+env.rda<-rda(spp.h,env.red,spatial.red)
 head(summary(env.rda)) ### Please observe the explanation of each axis.
+#plot(env.rda)
 spenvcor(env.rda) # species-environment correlation
 intersetcor(env.rda) #"interset" correlation
-envfit(env.rda,env.red) #fits environmental vectors or factors onto an ordination
-test.env<-anova(env.rda, permutations = how(nperm=999))
-test.env
-#plot(env.rda)
 
 
 ###########################################################################
-######## Testing the spatial significance, after considering ##############  
+######## Assessing the spatial model, after considering ##############  
 ################# the effect of selected env ##############################
 ###########################################################################
-spatial.rda<-rda(scores.pcoa.3,spatial.red,env.red)
+spatial.rda<-rda(spp.h,spatial.red,env.red)
 head(summary(spatial.rda)) ### Please observe the explanation of each axis.
+#plot(spatial.rda)
 spenvcor(spatial.rda) # species-space correlation
 intersetcor(spatial.rda) #"interset" correlation
-envfit(spatial.rda, spatial.red) #fits environmental vectors or factors onto an ordination
-test.spatial<-anova(spatial.rda, permutations = how(nperm=999))
-test.spatial
-#plot(spatial.rda)
 
 
 ###########################################################################
-### Evaluating the whole model ######
+#################### Evaluating the whole model ###########################
 ###########################################################################
 all.predictors <- cbind(env.red,spatial.red)
-all<-rda(scores.pcoa.3,all.predictors)
+all<-rda(spp.h,all.predictors)
+anova(all)
 head(summary(all)) ### Please observe the explanation of each axis.
-teste.all<-anova(all, permutations = how(nperm=999))
-teste.all
 #plot(all)
 spenvcor(all) # species-predictors correlation
 intersetcor(all) #"interset" correlation
-envfit(all, all.predictors) #fits environmental vectors or factors onto an ordination
 # To test each axis individually:
-rda.formula <- rda(scores.pcoa.3~., data=all.predictors)
+rda.formula <- rda(spp.h~., data=all.predictors)
 anova(rda.formula, by="axis")
+
+
+
+# RDA's graphs
+#all<-rda(spp.h,all.predictors)
+FACTOR.df<-read.table(file.choose(),row.names=1,header=T,sep=",")
+all.f<-cbind(FACTOR.df, all.predictors) ### all.predictors + FACTOR
+dim(all.f)
+View(all.f)
+write.table(all.f,"All_butFACTOR.csv")
+rda.graph<-all.f[,1]
+View(rda.graph) #Factor
+cca.results <- all
+cca.summary <- summary(cca.results)
+(imp.axis.1 <- cca.summary$cont$importance[2,1]) # explica??o do eixo 1
+(imp.axis.2 <- cca.summary$cont$importance[2,2]) # explica??o do eixo 2
+(rowScores <- as.data.frame(cca.results$CCA$u))  # scores das parcelas
+write.table(rowScores,"scores_parcelas.csv")
+(colScores <- as.data.frame(cca.results$CCA$v))  # scores das esp?cies 
+write.table(colScores,"scores_spp.csv")
+(bi.var <- as.data.frame(cca.results$CCA$biplot)) # scores das vari?veis ambientais
+write.table(bi.var,"scores_amb.csv")
+rowScores$FACTOR<-rda.graph
+str(rowScores)
+
+# tiff(filename="figura.tiff", res=600, height=600/72*600, width=600/72*600, compression= "lzw")
+
+
+
+library(ggplot2)
+ggrda1 <- ggplot() +
+    scale_shape_manual(values=c(16,17,18,19,20,21,17,19,20)) + # os numeros sao os tipos de pontos
+    geom_point(data=rowScores, aes(x=RDA1, y=RDA2, shape=FACTOR, colour=FACTOR)) +
+  geom_vline(xintercept = 0, alpha=0.9, linetype = "dashed") +
+  geom_hline(yintercept = 0, alpha=0.9, linetype = "dashed") +
+  geom_point(data = rowScores, aes(x = RDA1, y = RDA2, shape=FACTOR, colour=FACTOR),
+             size = 3,
+             fill="grey50") +
+  theme(legend.justification=c(0,0), legend.position=c(0.05,0.65),legend.key.size = unit(0.6, "cm"))+
+  theme(panel.background = element_rect(fill = "white", colour = NA),
+        panel.grid.minor = element_line(colour = NULL),
+        panel.grid.major = element_line(colour = NULL),
+        plot.background = element_rect(fill = "white", colour = NA),
+        panel.border = element_rect(fill = NA, colour = "black"),
+        axis.text.x = element_text(color ="black", size = 12, angle = 0),
+        axis.text.y = element_text(color ="black", size = 12, angle = 0))
+ggrda1
+
+#ggrda1 <- ggplot() +
+#     geom_point(data=rowScores, aes(x=RDA1, y=RDA2, shape=FACTOR, colour=FACTOR),
+#                size=2,
+#                fill = "white"
+#                  ) +
+#    geom_vline(xintercept = 0, alpha=0.9, linetype = "dashed") +
+#    geom_hline(yintercept = 0, alpha=0.9, linetype = "dashed") +
+#    geom_point(data = rowScores, aes(x = RDA1, y = RDA2, shape=FACTOR, colour=FACTOR),
+#               size = 3,
+#                fill="grey50") +
+#    theme(legend.justification=c(0,0), legend.position=c(0.05,0.65),legend.key.size = unit(0.6, "cm"))+
+#    theme(panel.background = element_rect(fill = "white", colour = NA),
+#          panel.grid.minor = element_line(colour = NULL),
+#          panel.grid.major = element_line(colour = NULL),
+#          plot.background = element_rect(fill = "white", colour = NA),
+#          panel.border = element_rect(fill = NA, colour = "black"),
+#          axis.text.x = element_text(color ="black", size = 12, angle = 0),
+#          axis.text.y = element_text(color ="black", size = 12, angle = 0))
+#ggrda1
+
+
+ggrda2 <- ggrda1 +
+    xlab(paste("RDA 1 ", "(", round(imp.axis.1*100, digits = 1), "%)")) +
+    theme(axis.title.x=element_text(angle = 0, size = 15)) + #, face = "bold"
+    ylab(paste("RDA 2 ", "(", round(imp.axis.2*100, digits = 1), "%)")) +
+    theme(axis.title.y=element_text(angle = 90, size = 15)) #, face = "bold"
+
+# Visualizar
+ggrda2
+
+
+
+
+ggrda3 <- ggrda2 +
+    geom_segment(data = bi.var, aes(x = 0, y = 0, xend = RDA1, yend = RDA2),
+                 size = 1,
+                 arrow = arrow(length = unit(0.2, "cm"),
+                               type = "closed", angle = 15),
+                 color = "black",
+                 alpha = 0.5) +
+    geom_text(data = bi.var, aes(RDA1*1.05, RDA2*1.05,
+                                 label = rownames(bi.var)),
+              color = "black",
+              size = 2,
+              fontface = "bold")
+#geom_text_repel(aes(x = colScores$PC1, y = colScores$PC2, label = rownames(colScores)))
+
+# Visualizar
+ggrda3
+
+
+
+ggrda4 <- ggrda3 + ggsave("RDA_MOD.tiff",width = 15, height = 15, units = "cm")
+
+# Visualizar
+ggrda4
 
 
 save.image()
